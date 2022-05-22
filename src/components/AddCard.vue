6202cc4a880560a71febc4ba792a030bed8e7324
Andrii Sokyrko <sokyrko.andrii@gmail.com>
2022-05-22 22:26:14 +0300

    initial deploy  furniture v15

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -32,10 +32,11 @@
       <!-- block NAME -->
       <div class="row">
           <div  class="col-9 p-2 d-flex align-middle justify-content-between">
-            <h3 >NAME</h3><input id="name" type = "check" v-model="name" placeholder="Please, provide name" v-on="handlers">
+            <h3 >NAME</h3>
+            <input id="name" type = "text" v-model="name" placeholder="Please, provide name" v-on="handlers">
           </div>
         </div><!-- row -->
-      <div class="row justify-content-end" :class = "{'visible': errors[1].id==='price' && !errors[1].check}">
+      <div class="row justify-content-end" :class = "{'visible': errors[1].id==='name' && !errors[1].check}">
         <div class="col-9" >{{errors[1].type}}</div>
       </div >
       <!-- block PRICE -->
@@ -68,7 +69,7 @@
             <input id="size" type = "text"   v-model="size" placeholder="Please, provide size" v-on="handlers">
           </div>
         </div>
-      <div class="row justify-content-end" :class = "{'visible': errors[3].id==='cise' && !errors[3].check}">
+      <div class="row justify-content-end" :class = "{'visible': errors[3].id==='size' && !errors[3].check}">
         <div class="col-9" >{{errors[3].type}}</div>
       </div >
 
@@ -81,8 +82,8 @@
               <input id = "height" type = "text" v-model="height" placeholder="Please, provide height"  v-on="handlers">
             </div >
           </div>
-          <div class="row justify-content-end" :class = "{'visible': errors[4].id==='height' && !errors[4].check}">
-            <div class="col-9" >{{errors[4].type}}</div>
+          <div class="row justify-content-end" :class = "{'visible': errors[5].id==='height' && !errors[5].check}">
+            <div class="col-9" >{{errors[5].type}}</div>
           </div >
         <!-- WIDTH -->
         <div class="row" >
@@ -91,8 +92,8 @@
               <input id = "width" type = "text" v-model="width" placeholder="Please, provide width"  v-on="handlers">
             </div >
           </div >
-          <div class="row justify-content-end" :class = "{'visible': errors[5].id==='width' && !errors[5].check}">
-            <div class="col-9" >{{errors[5].type}}</div>
+          <div class="row justify-content-end" :class = "{'visible': errors[6].id==='width' && !errors[6].check}">
+            <div class="col-9" >{{errors[6].type}}</div>
           </div >
         <!-- LENGTH -->
         <div class="row">
@@ -101,8 +102,8 @@
               <input id = "length" type = "text" v-model="length" placeholder="Please, provide length"  v-on="handlers">
             </div>
           </div >
-          <div class="row justify-content-end" :class = "{'visible': errors[6].id==='length' && !errors[6].check}">
-            <div class="col-9" >{{errors[6].type}}</div>
+          <div class="row justify-content-end" :class = "{'visible': errors[7].id==='length' && !errors[7].check}">
+            <div class="col-9" >{{errors[7].type}}</div>
           </div >
         </div><!-- // furniture-->
 
@@ -113,8 +114,8 @@
           <input id="weight" type = "text" v-model="weight" placeholder="Please, provide weight"  v-on="handlers">
         </div>
       </div><!-- row -->
-      <div class="row justify-content-end" :class = "{'visible': errors[7].id==='weight' && !errors[7].check}">
-        <div class="col-9" >{{errors[7].type}}</div>
+      <div class="row justify-content-end" :class = "{'visible': errors[8].id==='weight' && !errors[8].check}">
+        <div class="col-9" >{{errors[8].type}}</div>
       </div >
 
       <!-- /.row -->
@@ -151,7 +152,7 @@ export default {
       },
       errors: [
         {id: 'sku', check: '', type: '', correct: true, checkTypeProduct:''},
-        {id: 'name', check: '', type: '', correct: false, checkTypeProduct:''},
+        {id: 'name', check: '', type: '', correct: true, checkTypeProduct:''},
         {id: 'price', check: '', type: '', correct: true, checkTypeProduct:''},
         {id: 'furniture', check: '', type: '', correct: true, checkTypeProduct:''},
         {id: 'size', check: '', type: '', correct: false, checkTypeProduct:'DVD'},
@@ -181,42 +182,32 @@ export default {
       let l = document.getElementById('length').value
       this.errors[3].type='';
       this.errors[3].check = false;
-      // this.errors[5].type='';
-      // this.errors[6].type='';
-      // this.errors[7].type='';
-      // this.errors[5].check = false;
-      // this.errors[6].check = false;
-      // this.errors[7].check = false;
       for(let i =0; i<allCards.length;i++){
         let elm = allCards[i];
         if(elm.productType === 'Furniture'){
           if(elm.height === Number(h)  && elm.width===Number(w) && elm.length ===Number(l)){
             this.errors[3].type='Please, provide Firniture';
             this.errors[3].check = true;
-
-            // this.errors[5].type='Please, provide height';
-            // this.errors[6].type='Please, provide width';
-            // this.errors[7].type='Please, provide length';
-            // this.errors[5].check = true;
-            // this.errors[6].check = true;
-            // this.errors[7].check = true;
             break;
           }
         }
       }
     },
-    checkName(elm,data){
+    checkName(e){
+      let elm = this.errors[1];
       elm.check = false;
       elm.type = '';
+        let data = e.target.value;
         let result = this.getAllCards.filter( elm => {
           if(elm.name === data){
             return elm;
           }
         })
-        if(result.length){
+        if(result.length || data===''){
           elm.check = true;
-          elm.type = 'Please, submit required data';
+          elm.type = 'Please, provide name';
         }
+        console.log(this.errors);
     },
     checkChars(elm, id, data){
       if(elm.id === id && !isNaN(data)===true ) {
@@ -236,16 +227,16 @@ export default {
     },
     checkData(e){
       this.checkFurniture();
-
       document.getElementById('addCard').disabled = true;
       let id = e.target.id;
       let data = e.target.value;
 
       this.errors.forEach(elm =>{
-        if(elm.id ==='name'){
-          this.checkName(elm, data)
-        } else {
-          this.checkChars(elm,id,data);
+        if(id ==='name'){
+          this.checkName(e)
+        }
+       else {
+          this.checkChars(elm, id, data);
         }
       })
       let countCorrectElm = this.errors.filter(elm=>{
@@ -257,20 +248,18 @@ export default {
             }
         return acc;
       },0)
-      if(countCorrectElm.length === acc ){
-        let l = countCorrectElm.length
-        for (let i = 0; i<l;i++){
-         if(countCorrectElm[i].check===true){
-           break;
-         }
-        }
-        if(acc === 2){
-          document.getElementById('addCard').disabled = true;
 
-        } else {
-         this.checkFurniture();
-          document.getElementById('addCard').disabled = false;
-        }
+      if(countCorrectElm.length === acc ){
+        // let l = countCorrectElm.length
+        // for (let i = 0; i<l;i++){
+        //  if(countCorrectElm[i].check===true){
+        //    break;
+        //  }
+        document.getElementById('addCard').disabled = false;
+        // }
+        // if(acc === 3){
+        //   document.getElementById('addCard').disabled = true;
+        // }
       }
      },
     addCard(e){
@@ -292,15 +281,11 @@ export default {
     },
     changTypeProduct(){
       document.getElementById('addCard').disabled = true;
-
       let param = this.productType;
       Object.keys(this.checkTypeProduct).forEach(key => this.checkTypeProduct[key]=false)
       this.checkTypeProduct[param] =true;
-      this.errors.forEach((elm, ind) =>{
-        if(ind>2){
-          elm.correct = false;
-        }
-        if(elm.checkTypeProduct=== param){
+      this.errors.forEach((elm) => {
+        if(elm.checkTypeProduct === param){
           elm.correct = true;
         }
       })
